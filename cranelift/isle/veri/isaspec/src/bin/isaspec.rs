22f6d77a599dae141152907afd10889a64a39384
Alexa VanHattum <amv94@cornell.edu>
2024-10-30 22:53:34 -0400

    isaspec: spec and primitive to verify `fdiv` (#229)
    ASLp and macro for `fdiv`.
    
    Updates #224

--- cranelift/isle/veri/isaspec/src/bin/isaspec.rs
+++ cranelift/isle/veri/isaspec/src/bin/isaspec.rs
@@ -1880,7 +1880,7 @@ fn flags_mappings() -> Mappings {
 // MInst.FpuRRR specification configuration.
 fn define_fpu_rrr() -> SpecConfig {
     // FPUOp2
-    let fpu_op2s = [FPUOp2::Add, FPUOp2::Sub, FPUOp2::Mul];
+    let fpu_op2s = [FPUOp2::Add, FPUOp2::Sub, FPUOp2::Mul, FPUOp2::Div];
 
     // ScalarSize
     let sizes = [ScalarSize::Size32, ScalarSize::Size64];
