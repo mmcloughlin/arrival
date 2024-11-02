b6d683732c8ca333f60403496df1b6f0984bfd66
Michael McLoughlin <mcloughlin@cmu.edu>
2024-11-02 17:09:30 -0400

    meta: wasm to clif mapping dataset (#241)
    Derive mapping of wasm operators to CLIF instructions.
    
    * Dump Wasm operators and their corresponding proposals from the
    wasmparser table into `wasmoperators.csv`
    * Parse the `code_translator.rs` switch statement into a mapping from
    operators to target CLIF instructions `wasm2clif.json`
    * Combine: generate a list of target CLIF instructions you get when you
    map from MVP wasm operators and write to `cliftags.json`

---
+++ cranelift/isle/veri/meta/script/generate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Wasm operators
+wasmops="data/wasmoperators.csv"
+cargo run --bin wasmoperators >"${wasmops}"
+
+# Translation
+wasm2clif="data/wasm2clif.json"
+./script/wasm2clif.py --wasm-ops "${wasmops}" --output "${wasm2clif}"
+
+# Tagging
+cliftags="data/cliftags.json"
+./script/cliftags.py --data "${wasm2clif}" --output "${cliftags}"
--- cranelift/isle/veri/meta/src/bin/wasmoperators.rs
+++ cranelift/isle/veri/meta/src/bin/wasmoperators.rs
@@ -3,9 +3,9 @@ use wasmparser::for_each_operator;
 macro_rules! print_operator {
     ($( @$proposal:ident $op:ident $({ $($arg:ident: $argty:ty),* })? => $visit:ident)*) => {
         $(
-            println!("{}\t{}",
-                stringify!($proposal),
+            println!("{},{}",
                 stringify!($op),
+                stringify!($proposal),
             );
         )*
     }
