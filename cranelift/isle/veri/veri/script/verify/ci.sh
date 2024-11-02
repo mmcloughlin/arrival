7fa039a7b58949f98264622006064548d220b474
Michael McLoughlin <mcloughlin@cmu.edu>
2024-11-02 17:24:36 -0400

    coverage: slow tag disabled in ci (#242)
    Introduce a `slow` tag and exclude it in CI.
    
    Currently applied to the `load` and `store` CLIF terms, which generate a
    large number of expansions.

--- cranelift/isle/veri/veri/script/verify/ci.sh
+++ cranelift/isle/veri/veri/script/verify/ci.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 ./script/veri.sh "$@" -- \
     --filter include:first-rule-named \
     --filter exclude:tag:vector \
-    --filter exclude:tag:broken \
+    --filter exclude:tag:slow \
     ;
