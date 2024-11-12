95b5c56b88c6fa8080eba2fea3fdcbbf93ed794f
Michael McLoughlin <mcloughlin@cmu.edu>
2024-11-11 20:54:35 -0500

    coverage: drop `exclude:not:specified` mvp filter
     Updates #260

--- cranelift/isle/veri/veri/script/verify/mvp.sh
+++ cranelift/isle/veri/veri/script/verify/mvp.sh
@@ -9,10 +9,11 @@ RUST_LOG=info ./script/veri.sh \
     --results-to-log-dir \
     --num-threads 0 \
     --filter include:tag:wasm_proposal_mvp \
+    --filter exclude:tag:wasm_category_stack \
     --filter exclude:not:root:lower \
-    --filter exclude:not:specified \
     --filter exclude:tag:vector \
     --filter exclude:tag:i128 \
     --filter exclude:tag:atomics \
+    --filter exclude:tag:spectre \
     --filter exclude:tag:narrowfloat \
     ;
