f3c0d46f211098a2373d5131c776f1cafb2eda9b
Michael McLoughlin <mcloughlin@cmu.edu>
2024-11-12 18:39:34 -0500

    eval: disable fconst instructions

--- cranelift/isle/veri/veri/script/verify/mvp.sh
+++ cranelift/isle/veri/veri/script/verify/mvp.sh
@@ -16,4 +16,6 @@ RUST_LOG=info ./script/veri.sh \
     --filter exclude:tag:atomics \
     --filter exclude:tag:spectre \
     --filter exclude:tag:narrowfloat \
+    --filter exclude:tag:clif_f32const \
+    --filter exclude:tag:clif_f64const \
     ;
