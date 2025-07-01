f604fb28c7611af14ea68fbe9e88577823a247ac
Michael McLoughlin <mcloughlin@cmu.edu>
2025-07-01 17:48:23 -0400

    veri: ensure spec lines are sorted

--- cranelift/isle/veri/veri/script/spec_lines.sh
+++ cranelift/isle/veri/veri/script/spec_lines.sh
@@ -6,4 +6,4 @@ RUST_LOG=isle_spec_lines=trace cargo run --bin spec_lines -- \
     --codegen-crate-dir ../../../codegen/ \
     --work-dir /tmp \
     --name aarch64 \
-    2>&1 | grep SPEC_LINES
+    2>&1 | grep SPEC_LINES | sort
